3764b9b61220eeb8921fe8940c29d48e4dbbc1a1
Mateus Sarmento <omateusmento@gmail.com>
2023-02-13 14:23:10 -0300

    Inlcude dev server and html plugin in  webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,22 @@
 import { Configuration } from "webpack";
+import "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   entry: "./src/index.js",
   output: {
     path: __dirname + "/dist",
     filename: "index.js"
-  }
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: "auto",
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+  ],
 } as Configuration;
